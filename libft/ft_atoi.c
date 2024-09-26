001caed747b3a247a1477fac19bcb87e95aed791
Bytegab <gabrielfreiresimon@gmail.com>
2024-09-26 16:54:43 +0200

    change atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 19:13:12 by gafreire          #+#    #+#             */
-/*   Updated: 2024/09/25 19:13:12 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/09/26 16:54:35 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,7 @@ int ft_atoi(char *str)// add const char
     i = 0;
     sign = 1;
     result = 0;
-    while(str[i] == ' ' || str[i] == '\t' || str[i] == '\r' 
-    || str[i] == '\n' || str[i] == '\f' || str[i] == '\v')
-    i++;
-    while (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-')
-        {
-            sign = -sign;
-        }
-        i++;
-    }
+    
     while (str[i] >= '0' && str[i] <= '9')
     {
         result = (str[i] - '0') + (result * 10);
