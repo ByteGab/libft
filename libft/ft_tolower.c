a73b67c4fa35e0a4ce6ece7f88c18d99cf49b1d9
Bytegab <gabrielfreiresimon@gmail.com>
2024-09-29 20:24:08 +0200

    change ft_lower

--- libft/ft_tolower.c
+++ libft/ft_tolower.c
@@ -9,3 +9,24 @@
 /*   Updated: 2024/09/25 19:53:50 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+int ft_tolower(int c)
+{
+    if (c >= 65 && c <= 90)
+    {
+        return (c + 32);
+    }
+    return ('c');
+}
+
+int main (void)
+{
+    int c;
+
+    c = 'F';
+    c = ft_tolower(c);
+    write(1, &c, 1);
+    return(0);
+}
