47e214b5b1d8f3fb6d365c2a164848855053a27b
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-01 19:51:46 +0200

    change ft_memset

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -10,10 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <string.h> // use function ft_strlen --
 #include <stddef.h>
 
 void *ft_memset(void * s, int c, size_t n)
 {
-    
+    size_t i;
+    char *schar; 
+
+    schar = (char *)s; 
+    i = 0;
+    while(i < n)
+    {
+        schar[i] = (unsigned char)c;
+        i++;
+    }
+    return s;
 }
 
+int main(void)
+{
+    char str[50] = "Hello World!";
+    ft_memset(str, '-', 3);
+    write(1, str, strlen(str));
+    return (0);
+}
