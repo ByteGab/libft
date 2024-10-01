bc6cfe53ee62d526d24b5b0271a36f9cefed5ae6
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-01 20:57:19 +0200

    change ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -10,9 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <string.h> // use function ft_strlen -- change for my version
 #include <stddef.h>
 
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
+    size_t i;
+    char *destchar;
+    char *srchar;
 
+    i = 0;
+    destchar = (char*)dest;
+    srchar = (char*)src;
+    while(i < n)
+    {
+        destchar[i] = srchar[i];
+        i++;
+    }
+    return dest;
+}
+
+int main(void)
+{
+    char src[50] = "Hello World!";
+    char dest[50] = "Bye World!";// revision !\0 (puede ser que haga falta)
+    ft_memcpy(dest, src, 5);
+    write(1, dest, strlen(dest));
+    return (0);
 }
