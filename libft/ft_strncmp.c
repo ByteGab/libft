3ff3c0e8ef67b5197f7e4c48df2b9e1a23538672
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-02 19:56:44 +0200

    change ft_strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -10,9 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int strncmp (const char *s1, const *s2, unsigned int n) // change variabe "n"
+#include <stddef.h>
+
+int ft_strncmp (const char *s1, const *s2, size_t n) // change variabe "n"
 {
-    unsigned int    i;
+    size_t  i;
 
     i = 0;
     while (s1[i] != '\0' && s2[i] != '\0' && s1 == s2[i] == s2[i] && i < n)
