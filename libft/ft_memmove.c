85ecfb2df2bc7a8aeb1df3a278207820115fbc37
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-02 19:10:10 +0200

    change ft_memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -10,9 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stddef.h>
+#include <string.h>
 
 void *ft_memmove(void *dest, const void *src, size_t n)
 {
-    
+    size_t i;
+    char *destchar;
+    char *srchar;
+    char *temporal;
+
+    i = 0;
+    destchar = (char*)dest;
+    srchar = (char*)src;
+    while (i < n)
+    {
+        temporal[i] = srchar[i];
+        i++;
+    }
+    while (destchar[i] != '\0')
+    {
+        destchar[i] = temporal[i];
+        i++;
+    }
+    return dest;
+}
+
+int main(void)
+{
+    char src[10] = "123456789";
+    ft_memmove(src, src, 5);
+    write(1, src, strlen(src));
+    return (0);
 }
