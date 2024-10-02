c59bf981cc20a1dcc5b9b48ab9c1d051f507a10e
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-02 20:03:46 +0200

    change ft_strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -10,3 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
+char * ft_strnstr(const char *big, const char * little, size_t len)
+{
+
+}
