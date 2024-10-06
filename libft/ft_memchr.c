1e6d16536c5fce10e54d3e57d9c75e393cc92541
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-06 10:00:46 +0200

    change ft_memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -10,3 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h";
+
+void *ft_memchr(const void *str, int c, size_t n)
+{
+    unsigned char search;
+    
+}
