45f0b22fbbb638e0c027ed1501b1e6c196a9cef4
ByteGab <gabrielfreiresimon@gmail.com>
2024-10-09 22:08:09 +0200

    change ft_isprint

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -14,25 +14,22 @@
 
 int ft_isprint(int c)
 {
-    unsigned char cChar;
-
-    cChar = (unsigned char) c;
-    if(!(cChar >= 32 && cChar <= 126))
+    if((c >= 32 && c <= 126))
     {
-        return (0); // no es printable
+        return (1);
     }
-    return (1); // si es printable
+    return (0);
 }
 
 
 int main(void)
 {
     int	c;
-    int c2;
     
     c = '\0';
-	c2 = ft_isprint(c) + '0';
-	write(1, &c2, 1);
+	c = ft_isprint(c);
+    c += '0'; 
+	write(1, &c, 1);
 	return (0);
 }
 
