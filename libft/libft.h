78ff1862e1fbe7de271aa3cf872216abf336ea9c
ByteGab <gabrielfreiresimon@gmail.com>
2024-10-10 21:34:58 +0200

    change libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBFT_H
+#define LIBFT_H
 #include <unistd.h>
 int ft_isalpha(int c);
 int ft_isdigit(int c);
@@ -22,3 +24,5 @@ void bzero(void *s, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
 size_t ft_strlcpy (char *dst, const char *src, size_t size);
+
+#endif
