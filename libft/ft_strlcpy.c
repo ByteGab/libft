1cae09414b822711dad66d3dad8207399fae6095
ByteGab <gabrielfreiresimon@gmail.com>
2024-10-11 22:33:03 +0200

    change ft_strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -12,24 +12,11 @@
 
 #include "libft.h"
 
-int ft_strlen(char *s)
-{
-    int i;
-    i = 0;
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    return (i);
-}
-
 size_t ft_strlcpy (char *dst, const char *src, size_t size)
 {
     size_t i;
-    size_t len;
 
     i = 0;
-    len = ft_strlen(src);
     if (size != 0)
     {
         while (src[i] != '\0' && i < size -1)
@@ -39,6 +26,6 @@ size_t ft_strlcpy (char *dst, const char *src, size_t size)
         }
         dst[i] = '\0';
     }
-    return (len);
+    return (i);
 
 }
