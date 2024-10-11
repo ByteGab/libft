1b3e1c3daef99f67f4b43359950933facf7c6f1a
ByteGab <gabrielfreiresimon@gmail.com>
2024-10-11 19:50:15 +0200

    change format ft_isalnum

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -12,25 +12,23 @@
 
 #include "libft.h"
 
-int ft_isalnum(int c)
-{    
-    if (((c >= 65 && c <= 90)
-        || (c >= 97 && c <= 122))
-        || (c >= 48 && c <= 57))
-    {
-        return (1);
-    }
-        return (0);
+int	ft_isalnum(int c)
+{
+	if (((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) || (c >= 48 && c <= 57))
+	{
+		return (1);
+	}
+	return (0);
 }
 
-
-int main(void)
+int	main(void)
 {
-    int	c;
-    
-    c = 'b';
+	int	c;
+
+	c = 'b';
 	c = ft_isalnum(c);
-    c += '0'; 
+	c += '0';
 	write(1, &c, 1);
 	return (0);
 }
+   
