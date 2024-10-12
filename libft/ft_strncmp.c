d31f51bd505bc4af966caad50765b1e188717a99
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-12 19:00:16 +0200

    change ft_strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 19:53:20 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/03 16:46:42 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/12 18:43:41 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,4 @@ int ft_strncmp (const char *s1, const *s2, size_t n) // change variabe "n"
         i++;
     }
     return (s1[i] - s2[i]);
-
 }
