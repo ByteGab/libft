3bf60b83d32c3405579f4aff14658c330dfad203
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-12 20:00:07 +0200

    change ft_memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 10:05:24 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/06 10:05:24 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/12 19:55:05 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void * ft_memchr(const void *str, int c, size_t n)
 {
-
+    size_t i;
+    char *strchar = (char)str;
+    while(str[i] != '\0' && i < n)
+    {
+        
+    }
 }
