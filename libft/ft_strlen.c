aa996511fa9e567849616ad6c325b88ff80b572f
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 01:31:46 +0200

    ft_strlen

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,23 +6,23 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:26:37 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/08 16:29:46 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/12 21:56:48 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-    size_t i;
-    i = 0;
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    return (i);
+	size_t i;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
-
+/*
 int main() {
     const char *str = "Hola, mundo!";
     size_t length = ft_strlen(str);
@@ -30,3 +30,4 @@ int main() {
     printf("La longitud de la cadena es: %zu\n", length);
     return 0;
 }
+*/
