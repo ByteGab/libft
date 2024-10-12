16e40a1f671e58e4fba0d86bd75aded827823ab4
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-12 20:05:27 +0200

    change ft_isalpha

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,21 +6,21 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:24:40 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/08 16:10:17 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/12 20:04:42 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int     ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
-        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-        {
-                return (1);
-        }
-        return (0);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
 }
-
+/*
 int     main(void)
 {
         char    c;
@@ -31,4 +31,4 @@ int     main(void)
         write(1, &c, 1);
         return (0);
 }
-   
+*/
