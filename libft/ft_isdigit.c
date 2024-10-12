ae9ee6291bd03bd537d1d3fc566987c6cbce66fc
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-12 21:48:57 +0200

    change ft_isdigit

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:25:01 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/08 16:11:44 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/12 21:48:19 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_isdigit(int c)
 	}
 	return (0);
 }
-
+/*
 int	main(void)
 {
 	int	c;
@@ -31,4 +31,4 @@ int	main(void)
 	write(1, &c, 1);
 	return (0);
 }
-   
+*/
