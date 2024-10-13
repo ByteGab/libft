ecaa8d665f78384c174b199a0da52a8c91738150
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 17:01:25 +0200

    change ft_memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,24 +6,25 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 03:33:02 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/13 01:26:02 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 17:01:08 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int memcmp(const void *str1, const void * str2, size_t n)
+int	memcmp(const void *str1, const void *str2, size_t n)
 {
-    size_t i;
-    unsigned char *str1char;
-    unsigned char *str2char;
-    
-    i = 0;
-    str1char = (unsigned char*)str1;
-    str2char = (unsigned char*)str2;
-    while (str1char[i] != '\0' && str2char[i] != '\0' && str1char[i] == str2char[i] && i < n)
-    {
-        i++;
-    }
-    return (str1char[i] - str2char[i]);
+	size_t			i;
+	unsigned char	*str1char;
+	unsigned char	*str2char;
+
+	i = 0;
+	str1char = (unsigned char *)str1;
+	str2char = (unsigned char *)str2;
+	while (str1char[i] != '\0' && str2char[i] != '\0'
+		&& str1char[i] == str2char[i] && i < n)
+	{
+		i++;
+	}
+	return (str1char[i] - str2char[i]);
 }
