cf3adb72fc21cd84f496c15c08c146d84a3db8a7
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 16:59:46 +0200

    change ft_strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,20 +6,20 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 19:53:20 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/12 22:29:36 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 16:59:20 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strncmp (const char *s1, const *s2, size_t n) // change variabe "n"
+int	ft_strncmp(const char *s1, const *s2, size_t n) // change variabe "n"
 {
-    size_t  i;
+	size_t i;
 
-    i = 0;
-    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n)
-    {
-        i++;
-    }
-    return (s1[i] - s2[i]);
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n)
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
