16c98208484e7ae14defa41f9c7416e5823b63c9
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 16:55:44 +0200

    change ft_strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,30 +6,30 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:26:21 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/12 13:20:30 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 16:55:14 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-char    ft_strlcat(char *dst, const char *src, size_t size)
+char	ft_strlcat(char *dst, const char *src, size_t size)
 {
-    size_t i;
-    size_t z;
-    size_t result;
+	size_t i;
+	size_t z;
+	size_t result;
 
-    i = 0;
-    z = 0;
-    while (dst[i] != '\0')
-    {
-        i++;
-    }
-    while (src[z] != '\0' && z < size)
-    {
-        dst[i + z] = src[z];
-        z++;
-    }
-    dst[i + z] = '\0';
-    result = i + z;
-    return (result);
+	i = 0;
+	z = 0;
+	while (dst[i] != '\0')
+	{
+		i++;
+	}
+	while (src[z] != '\0' && z < size)
+	{
+		dst[i + z] = src[z];
+		z++;
+	}
+	dst[i + z] = '\0';
+	result = i + z;
+	return (result);
 }
