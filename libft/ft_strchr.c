116d33b423f0f5c380e9f0fee73c9d3619e28b07
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 16:58:22 +0200

    change ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,44 +6,49 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:26:06 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/12 18:10:40 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 16:58:03 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-    int i;
-    int z;
+	int	i;
+	int	z;
 
-    i = 0;
-    while(s[i] != '\0')
-    {
-        i++;
-    }
-    i += 1;
-    while(z < i)
-    {
-        if(s[i] == (char)c)
-        {
-            return (char *)&s[i];
-        }
-        z++;
-    }
-    return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i += 1;
+	while (z < i)
+	{
+		if (s[i] == (char)c)
+		{
+			return ((char *)&s[i]);
+		}
+		z++;
+	}
+	return (NULL);
 }
+/*
+int	main(void)
+{
+	const char	*texto = "hola mundo";
+	char		*resultado;
 
-int main() {
-    const char *texto = "hola mundo";
-    char *resultado = ft_strchr(texto, '\0');
-
-    if (resultado != NULL) {
-        printf("Carácter encontrado: %c\n", *resultado);
-        printf("Posición: %ld\n", resultado - texto);
-    } else {
-        printf("Carácter no encontrado.\n");
-    }
-
-    return 0;
+	resultado = ft_strchr(texto, '\0');
+	if (resultado != NULL)
+	{
+		printf("Carácter encontrado: %c\n", *resultado);
+		printf("Posición: %ld\n", resultado - texto);
+	}
+	else
+	{
+		printf("Carácter no encontrado.\n");
+	}
+	return (0);
 }
+*/
