bd50c1ac59b811b42d861c08219ab983fbc92347
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 17:05:26 +0200

    change ft_strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,23 +6,23 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 16:16:56 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/13 16:24:33 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 17:05:10 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    size_t i;
-    size_t size;
-    char *mem;
-    
-    i = 0;
-    size = ft_strlen(s1);
-    mem = (char *)malloc(size + 1);
-    if(mem == NULL)
-        return NULL;
-    ft_strlcat(s1, s2, size);
-    return (s1);
+	size_t i;
+	size_t size;
+	char *mem;
+
+	i = 0;
+	size = ft_strlen(s1);
+	mem = (char *)malloc(size + 1);
+	if (mem == NULL)
+		return (NULL);
+	ft_strlcat(s1, s2, size);
+	return (s1);clear
 }
