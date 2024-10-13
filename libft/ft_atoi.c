7213108a38add7424c2c3ba3be77e21d0eae103e
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 17:02:30 +0200

    change ft_atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,38 +6,38 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 19:13:12 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/13 12:48:48 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 17:02:20 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
-    int i;
-    int sign;
-    int result;
+	int i;
+	int sign;
+	int result;
 
-    i = 0;
-    sign = 1;
-    result = 0;
-    
-    while(nptr[i] == "")
-    {
-        i++;
-    }
-    if(nptr[i] == '-')
-    {
-        i++;
-        while(nptr[i] >= '0' && nptr[i] <= '9')
-        {
-            result = result * 10 + nptr[i] - '0';
-            i++;
-        }
-        return(-result);
-    }
-    while (nptr[i] >= '0' && nptr[i] <='9')
-    {
-        result = result * 10 + nptr[i] - '0';
-        i++;
-    }
-    return(result);
+	i = 0;
+	sign = 1;
+	result = 0;
+
+	while (nptr[i] == "")
+	{
+		i++;
+	}
+	if (nptr[i] == '-')
+	{
+		i++;
+		while (nptr[i] >= '0' && nptr[i] <= '9')
+		{
+			result = result * 10 + nptr[i] - '0';
+			i++;
+		}
+		return (-result);
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + nptr[i] - '0';
+		i++;
+	}
+	return (result);
 }
