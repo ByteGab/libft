8d60d71dd5208946ba01c37b52b6714e0b5a7f18
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 17:04:17 +0200

    change ft_substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,31 +6,30 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 14:18:14 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/13 16:21:07 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 17:03:54 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    size_t i;
-    size_t size;
-    char *mem;
-    
-    
-    i = start;
-    size = ft_strlen(s);
-    mem = (char *)malloc(size + 1);
-    if(mem == NULL)
-        return NULL;
-    if(size - start < len)
-        len = size - start;
-    while(i < len)
-    {
-        mem[i] = s[i];
-        i++;
-    }
-    mem[i] = '\0';
-    return (mem);
+	size_t i;
+	size_t size;
+	char *mem;
+
+	i = start;
+	size = ft_strlen(s);
+	mem = (char *)malloc(size + 1);
+	if (mem == NULL)
+		return (NULL);
+	if (size - start < len)
+		len = size - start;
+	while (i < len)
+	{
+		mem[i] = s[i];
+		i++;
+	}
+	mem[i] = '\0';
+	return (mem);
 }
