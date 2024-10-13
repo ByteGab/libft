203b3ff9d30b61a41ee130059c02f9bd8522be07
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 17:00:52 +0200

    change ft_memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,26 +6,26 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 10:05:24 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/13 12:31:13 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 17:00:41 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void * ft_memchr(const void *str, int c, size_t n)
+void	*ft_memchr(const void *str, int c, size_t n)
 {
-    size_t i;
-    char *strchar;
-    
-    strchar = (char *)str;
-    i = 0;
-    while(strchar[i] != '\0' && i < n)
-    {
-        if(strchar[i] == (char)c)
-        {
-            return (strchar[i]);
-        }
-        i++;
-    }
-    return  (NULL);
+	size_t i;
+	char *strchar;
+
+	strchar = (char *)str;
+	i = 0;
+	while (strchar[i] != '\0' && i < n)
+	{
+		if (strchar[i] == (char)c)
+		{
+			return (strchar[i]);
+		}
+		i++;
+	}
+	return (NULL);
 }
