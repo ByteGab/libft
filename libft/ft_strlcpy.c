f1e226c3a4634a627083bf5a128cca67b886a09e
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 16:52:34 +0200

    change ft_strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,36 +6,39 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:26:28 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/12 13:14:28 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 16:52:15 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcpy (char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    if (size != 0)
-    {
-        while (src[i] != '\0' && i < size -1)
-        {
-            dst[i] = src[i];
-            i++;
-        }
-        dst[i] = '\0';
-    }
-    return (i);
-
+	i = 0;
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (i);
 }
-
-int main(void)
+/*
+int	main(void)
 {
-    size_t i = 10;
-    char src [5] = "adios";
-    char dest[10] = "hola";
-    char result = ft_strlcpy(dest, src, i);
-    write(1, result, 1);
-    return (0);
+	size_t	i;
+	char	dest[10] = "hola";
+	char	result;
+
+	i = 10;
+	char src [5] = "adios";
+	result = ft_strlcpy(dest, src, i);
+	write(1, result, 1);
+	return (0);
 }
+*/
