083279f131fd0db136e0ea4ed9b65a31ee043fb7
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 16:51:55 +0200

    change ft_memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,49 +6,51 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:25:53 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/12 12:55:43 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 16:51:44 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    size_t i;
-    char *destchar;
-    char *srchar;
+	size_t	i;
+	char	*destchar;
+	char	*srchar;
 
-    i = n;
-    destchar = (char*)dest;
-    srchar = (char*)src;
-    
-    if(srchar < destchar)
-    {
-        i = n;
-        while(i > 0)
-    {
-        i--;
-        destchar[i] = srchar[i];
-    }
-    }
-    else
-    {
-        i = 0;
-        while(i < n)
-        {
-            destchar[i] = srchar[i];
-            i++;
-        }      
-    }
-    return (dest);
+	i = n;
+	destchar = (char *)dest;
+	srchar = (char *)src;
+	if (srchar < destchar)
+	{
+		i = n;
+		while (i > 0)
+		{
+			i--;
+			destchar[i] = srchar[i];
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			destchar[i] = srchar[i];
+			i++;
+		}
+	}
+	return (dest);
 }
-
-int main(void)
+/*
+int	main(void)
 {
-    size_t i = 10;
-    char src [5] = "adios";
-    char dest[10] = "hola";
-    ft_memmove(dest, src, i);
-    write(1, dest, strlen(dest));
-    return (0);
+	size_t	i;
+	char	src[5] = "adios";
+	char	dest[10] = "hola";
+
+	i = 10;
+	ft_memmove(dest, src, i);
+	write(1, dest, strlen(dest));
+	return (0);
 }
+*/
