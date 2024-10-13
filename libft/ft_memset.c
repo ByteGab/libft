86623755da92fafa0eadd107a74dd81c9b624a48
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 14:47:17 +0200

    change ft_memset

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,27 +6,27 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 18:17:00 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/08 16:43:58 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 13:52:17 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memset(void * s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
-    size_t i;
-    unsigned char *schar; //change name variable // revision "unsigned char"
+	size_t	i;
+	unsigned char	*schar;
 
-    i = 0;
-    schar = (unsigned char *)s; 
-    while(i < n)
-    {
-        schar[i] = (unsigned char)c;
-        i++;
-    }
-    return s;
+	i = 0;
+	schar = (unsigned char *)s;
+	while (i < n)
+	{
+		schar[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
 }
-
+/*
 int main(void)
 {
     char str[50] = "Hello World!";
@@ -34,3 +34,4 @@ int main(void)
     write(1, str, strlen(str));
     return (0);
 }
+*/
