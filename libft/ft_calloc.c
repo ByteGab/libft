ab96fc73adb9d4600f958b322c165de3412c4541
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 17:02:56 +0200

    change ft_calloc

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,20 +6,20 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 16:34:36 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/13 13:29:28 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 17:02:45 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-    void *mem;
-    size_t sizeTotal;
-    
-    mem = malloc(nmemb * size);
-    if (malloc == NULL)
-        return (NULL);
-    ft_bzero(mem,sizeTotal);
-    return (mem);
+	void *mem;
+	size_t sizeTotal;
+
+	mem = malloc(nmemb * size);
+	if (malloc == NULL)
+		return (NULL);
+	ft_bzero(mem, sizeTotal);
+	return (mem);
 }
