f97d6c0c0a1635432977d27ee34250d9447e3fcf
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 12:28:01 +0200

    ft_putnbr_fd

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,28 +6,28 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 03:23:58 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/13 03:28:46 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 12:27:29 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_write(char char_nb)
+void	ft_write(char char_nb, int fd)
 {
-	write (1, &char_nb, 1);
+	write (fd, &char_nb, 1);
 }
 void  ft_putnbr_fd (int n, int fd)
 {
     if (n == -2147483648)
 	{
-		ft_write('-');
-		ft_write('2');
+		ft_write('-',fd);
+		ft_write('2',fd);
 		ft_putnbr(147483648);
 		return ;
 	}
 	if (n < 0)
 	{
-		ft_write('-');
+		ft_write('-',fd);
 		n = -n;
 	}
 	if (n > 9)
@@ -37,6 +37,6 @@ void  ft_putnbr_fd (int n, int fd)
 	}
 	else
 	{
-		ft_write(n + '0');
+		ft_write(n + '0',fd);
 	}
 }
