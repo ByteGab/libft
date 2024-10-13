82881c68a685134256f7a268366748c4011e85e4
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-13 03:40:23 +0200

    change ft_strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 19:53:31 by gafreire          #+#    #+#             */
-/*   Updated: 2024/09/25 19:53:31 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/13 03:07:36 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,40 @@
 
 char * ft_strnstr(const char *big, const char * little, size_t len)
 {
+    size_t  i;
+    size_t  z;
+    
+    i = 0;
+    z = 0;
+    if(!little)
+    {
+        return ((char *)big);
+    }
+    
 
+    while (big[i] != '\0' && i < len)
+    {
+        if(big[i + z] == *little)
+        {
+            while (big[i + z] == little[z])
+            {
+                z++;
+            }
+            return ((char *)&big[i]);
+        }
+        i++;
+    }
+    return (NULL);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    const char *big = "hola cacargkhhfs";
+    const char *little = "caca";
+    size_t len;
+    
+    printf("%s\n", ft_strnstr(big, little, 8));
+    return (0);
 }
