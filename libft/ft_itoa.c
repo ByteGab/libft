3e7ca7b790748a3bf8f7275a4a67aa4ebd45c74c
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-17 16:15:55 +0200

    change ft_itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -3,72 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gafreire <gafreire@student.42.fr>          #+#  +:+       +#+        */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-10-17 13:54:20 by gafreire          #+#    #+#             */
-/*   Updated: 2024-10-17 13:54:20 by gafreire         ###   ########.fr       */
+/*   Created: 2024/10/17 13:54:20 by gafreire          #+#    #+#             */
+/*   Updated: 2024/10/17 16:15:18 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static int      ft_counter(int n)
+static int	ft_counter(int n)
 {
-        int     count;
+	int	count;
 
-        count = 0;
-        if (n == 0)
-                return (1);
-        if (n != 0)
-        {
-                if (n < 0)
-                {
-                        count++;
-                        n = -n;
-                }
-                while (n != 0)
-                {
-                        n = n / 10;
-                        count++;
-                }
-        }
-        return (count);
+	count = 0;
+	if (n == 0)
+		return (1);
+	if (n != 0)
+	{
+		if (n < 0)
+		{
+			count++;
+			n = -n;
+		}
+		while (n != 0)
+		{
+			n = n / 10;
+			count++;
+		}
+	}
+	return (count);
 }
 
-char    *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-        size_t  len;
-        char    *str;
-        long    nbr;
+	size_t	len;
+	char	*str;
+	long	nbr;
 
-        nbr = n;
-        len = ft_counter(n);
-        str = malloc((len + 1));
-        if (!str)
-                return (0);
-        str[len--] = '\0';
-        if (nbr == 0)
-                str[0] = '0';
-        if (nbr < 0)
-        {
-                str[0] = '-';
-                nbr = -nbr;
-        }
-        while (nbr > 0)
-        {
-                str[len--] = (nbr % 10) + '0';
-                nbr /= 10;
-        }
-        return (str);
+	nbr = n;
+	len = ft_counter(n);
+	str = malloc((len + 1));
+	if (!str)
+		return (0);
+	str[len--] = '\0';
+	if (nbr == 0)
+		str[0] = '0';
+	if (nbr < 0)
+	{
+		str[0] = '-';
+		nbr = -nbr;
+	}
+	while (nbr > 0)
+	{
+		str[len--] = (nbr % 10) + '0';
+		nbr /= 10;
+	}
+	return (str);
 }
 
 /*
 #include <stdio.h>
 
-int main()
+int	main(void)
 {
-        int     numerito = 4242;
-        printf("%s\n", ft_itoa(numerito));
-        return(0);
+		int     numerito = 4242;
+		printf("%s\n", ft_itoa(numerito));
+		return(0);
 }*/
