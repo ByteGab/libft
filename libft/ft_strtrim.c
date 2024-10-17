9a00d8357cff5fe58492100a8d07d7c8ef163659
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-17 16:58:24 +0200

    chanfe ft_strim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 16:40:29 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/13 17:30:16 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/17 16:53:01 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	size;
 	char	*mem;
-	char*	hola;
+	char	*hola;
 
-	hola = (char*)set;
+	hola = (char *)set;
 	size = ft_strlen(s1);
 	mem = (char *)malloc(size + 1);
 	if (mem == NULL)
