662c9602022f30afb64d75fe2e1e124fe7a6e8bd
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-17 16:59:47 +0200

    change ft_substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,12 +6,13 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 14:18:14 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/13 17:28:36 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/17 16:58:44 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
