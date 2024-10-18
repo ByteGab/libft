d2ff6a90b3ae1630e9a4681951e3dbc981efdcf2
ByteGab <gabrielfreiresimon@gmail.com>
2024-10-18 23:18:19 +0200

    change ft_strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -15,11 +15,19 @@
 int	ft_strncmp(const char *s1, const char*s2, size_t n)
 {
 	size_t	i;
+	unsigned char char_s1;
+	unsigned char char_s2;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n)
+	while(i < n)
 	{
+		char_s1 = (unsigned char)s1[i];
+		char_s2 = (unsigned char)s2[i];
+		if((char_s1 == '\0' || char_s2 == '\0') || (char_s1 != char_s2))
+		{
+			return (char_s1 - char_s2);
+		}
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
