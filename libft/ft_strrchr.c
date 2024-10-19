ef44b89a106d92b48d86dbc068f54bdc1b4a07bd
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-19 12:00:27 +0200

    change ft_strrchr

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 19:53:40 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/13 16:58:47 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/19 12:00:06 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,38 @@
 
 char	*ft_strrchr(const char *str, int c)
 {
-	const char *last_occurrence;
-	    if (c == '\0') {
-        // Buscamos el final de la cadena y devolvemos ese puntero
-        while (*str != '\0') {
-            str++;
-        }
-        return (char *)str;  // Aquí `str` apunta al carácter nulo
-    }
+	const char	*last_occurrence;
 
-    // Contar la longitud de la cadena
-	last_occurrence = NULL;  // Puntero para la última ocurrencia
-    while (*str != '\0') {
-        if (*str == (char)c) {
-            last_occurrence = str;  // Guardamos la dirección de la última ocurrencia
-        }
-        str++;  // Avanzar al siguiente carácter
-    }
-
-    // Retornar la última ocurrencia, o NULL si no se encontró
-    return (char *)last_occurrence;
+	if (c == '\0')
+	{
+		// Buscamos el final de la cadena y devolvemos ese puntero
+		while (*str != '\0')
+		{
+			str++;
+		}
+		return ((char *)str); // Aquí `str` apunta al carácter nulo
+	}
+	// Contar la longitud de la cadena
+	last_occurrence = NULL; // Puntero para la última ocurrencia
+	while (*str != '\0')
+	{
+		if (*str == (char)c)
+		{
+			last_occurrence = str;
+				// Guardamos la dirección de la última ocurrencia
+		}
+		str++; // Avanzar al siguiente carácter
+	}
+	// Retornar la última ocurrencia, o NULL si no se encontró
+	return ((char *)last_occurrence);
 }
 /*
 int	main(void)
 {
-	const char *texto = "hola adios hola";
-	char *resultado = ft_strrchr(texto, 'h');
+	const char	*texto = "hola adios hola";
+	char		*resultado;
 
+	resultado = ft_strrchr(texto, 'h');
 	if (resultado != NULL)
 	{
 		printf("Carácter encontrado: %c\n", *resultado);
@@ -51,7 +56,6 @@ int	main(void)
 	{
 		printf("Carácter no encontrado.\n");
 	}
-
 	return (0);
 }
 */
