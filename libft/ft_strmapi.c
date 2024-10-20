bf4e5744782c8c6ed52f9a7136cedbf006983b68
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-20 12:02:18 +0200

    ft_mapi

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,19 +6,18 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 19:11:56 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/19 21:06:39 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/20 11:59:49 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t size;
-	size_t i;
-	char *result;
+	size_t	size;
+	size_t	i;
+	char	*result;
 
 	size = ft_strlen(s);
 	i = 0;
@@ -26,7 +25,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	{
 		return (NULL);
 	}
-
 	result = (char *)malloc(size + 1);
 	if (result == NULL)
 	{
