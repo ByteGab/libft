05ca9b4df48ddd9b9d4396c671e08990c06b6367
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-20 14:44:36 +0200

    change ft_strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:26:28 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/17 17:03:46 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/20 14:43:52 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,12 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	size_src;
 	size_t	i;
 
-	size_src = ft_strlen(src);
 	i = 0;
 	if (!src)
-	{
 		return (0);
-	}
+	size_src = ft_strlen(src);
 	if (size == 0)
-	{
 		return (size_src);
-	}
 	while (src[i] != '\0' && i < size - 1)
 	{
 		dst[i] = src[i];
