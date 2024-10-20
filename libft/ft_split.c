d9bcdccde6248341f86516205f6397f6c9e25d03
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-20 11:24:01 +0200

    change ft_split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:18:01 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/20 11:17:21 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/20 11:23:52 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,28 +37,23 @@ static size_t	count_words(char const *s, char c)
 	return (count);
 }
 
-static size_t	sizesub(char const *s, char c)
+static size_t	size_sub(char const *s, char c)
 {
 	size_t	i;
-	size_t	word;
 	size_t	count;
-	size_t	start;
 
 	i = 0;
-	word = 0;
 	count = 0;
-	start = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] != c && word == 0)
+		if (s[i] != c && s[i] != '\0')
 		{
-		}
-		else if (s[i] == c)
-		{
-			word = 0;
+			count++;
+			i++;
 		}
 		i++;
 	}
+	return (count);
 }
 
 char	**ft_split(char const *s, char c)
