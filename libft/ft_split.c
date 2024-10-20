7748c8f1ec7e46d0865baf3f36c51b18a702b204
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-20 11:18:15 +0200

    change ft_split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:18:01 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/20 11:07:39 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/20 11:17:21 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 static size_t	count_words(char const *s, char c)
 {
-	size_t		i;
-	size_t		word;
+	size_t	i;
+	size_t	word;
 	size_t	count;
 
 	i = 0;
 	word = 0;
 	count = 0;
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		if (s[i] != c && word == 0)
 		{
@@ -37,18 +37,37 @@ static size_t	count_words(char const *s, char c)
 	return (count);
 }
 
+static size_t	sizesub(char const *s, char c)
+{
+	size_t	i;
+	size_t	word;
+	size_t	count;
+	size_t	start;
+
+	i = 0;
+	word = 0;
+	count = 0;
+	start = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && word == 0)
+		{
+		}
+		else if (s[i] == c)
+		{
+			word = 0;
+		}
+		i++;
+	}
+}
+
 char	**ft_split(char const *s, char c)
 {
 	size_t	words;
 	char	**sub;
-	size_t	i;
 
 	words = count_words(s, c);
 	sub = (char **)malloc(sizeof(char *) * words);
-	while(s[i] != '\0')
-	{
-		
-	}
 	while (i < n)
 	{
 		free(sub[i]);
