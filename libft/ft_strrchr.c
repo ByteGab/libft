8d8c9274317ff368ad8acd3f50d13f610ee784d2
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-20 11:52:03 +0200

    change ft_strrchr

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 19:53:40 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/19 12:00:06 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/20 11:51:35 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,21 @@ char	*ft_strrchr(const char *str, int c)
 
 	if (c == '\0')
 	{
-		// Buscamos el final de la cadena y devolvemos ese puntero
 		while (*str != '\0')
 		{
 			str++;
 		}
-		return ((char *)str); // Aquí `str` apunta al carácter nulo
+		return ((char *)str);
 	}
-	// Contar la longitud de la cadena
-	last_occurrence = NULL; // Puntero para la última ocurrencia
+	last_occurrence = NULL;
 	while (*str != '\0')
 	{
 		if (*str == (char)c)
 		{
 			last_occurrence = str;
-				// Guardamos la dirección de la última ocurrencia
 		}
-		str++; // Avanzar al siguiente carácter
+		str++;
 	}
-	// Retornar la última ocurrencia, o NULL si no se encontró
 	return ((char *)last_occurrence);
 }
 /*
