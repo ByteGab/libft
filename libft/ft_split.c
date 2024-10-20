08a93b1c0b04cfdc147c60b53df555c1be0fc623
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-20 11:10:13 +0200

    change ft_split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:18:01 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/20 10:59:07 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/20 11:07:39 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 static size_t	count_words(char const *s, char c)
 {
-	int		i;
-	int		word;
+	size_t		i;
+	size_t		word;
 	size_t	count;
 
 	i = 0;
@@ -28,7 +28,7 @@ static size_t	count_words(char const *s, char c)
 			count++;
 			word = 1;
 		}
-		else if (s[i] == c || s[i + 1] == c)
+		else if (s[i] == c)
 		{
 			word = 0;
 		}
@@ -41,12 +41,14 @@ char	**ft_split(char const *s, char c)
 {
 	size_t	words;
 	char	**sub;
+	size_t	i;
 
 	words = count_words(s, c);
 	sub = (char **)malloc(sizeof(char *) * words);
-	//strncpy(sub[i], inicio_subcadena,len);
-	//sub[i][len] = '\0';
-	// sub[i] = (char *)malloc(len + 1) * sizeof (char));
+	while(s[i] != '\0')
+	{
+		
+	}
 	while (i < n)
 	{
 		free(sub[i]);
