82e3bbf31bf4b87b3c6471896e63cdd23242060f
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-20 13:12:50 +0200

    change ft_memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,24 +6,18 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:25:53 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/20 12:47:44 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/20 13:12:33 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	copy_char(void *dest, const void *src, size_t n)
 {
 	size_t				i;
 	unsigned char		*char_dest;
 	const unsigned char	*char_src;
 
-	if (!dest && !src)
-		return (NULL);
-	char_dest = (unsigned char *)dest;
-	char_src = (const unsigned char *)src;
-	if (n == 0 || char_dest == char_src)
-		return (dest);
 	if (char_src < char_dest)
 	{
 		i = n;
@@ -42,6 +36,20 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 			i++;
 		}
 	}
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*char_dest;
+	const unsigned char	*char_src;
+
+	if (!dest && !src)
+		return (NULL);
+	char_dest = (unsigned char *)dest;
+	char_src = (const unsigned char *)src;
+	if (n == 0 || char_dest == char_src)
+		return (dest);
+	copy_char(char_dest, char_src, n);
 	return (dest);
 }
 /*
