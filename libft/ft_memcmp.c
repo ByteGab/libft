53d0f153efa8215074f94c14701089e8bd4eac8e
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-20 13:03:56 +0200

    change ft_memcpm

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 03:33:02 by gafreire          #+#    #+#             */
-/*   Updated: 2024/10/20 11:53:15 by gafreire         ###   ########.fr       */
+/*   Updated: 2024/10/20 13:02:42 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-	size_t			i;
-	unsigned char	*str1char;
-	unsigned char	*str2char;
+	size_t				i;
+	const unsigned char	*char_str1;
+	const unsigned char	*char_str2;
 
 	i = 0;
-	str1char = (unsigned char *)str1;
-	str2char = (unsigned char *)str2;
+	char_str1 = (const unsigned char *)str1;
+	char_str2 = (const unsigned char *)str2;
 	while (i < n)
 	{
-		if (str1char[i] != str2char[i])
+		if (char_str1[i] != char_str2[i])
 		{
-			return (str1char[i] - str2char[i]);
+			return (char_str1[i] - char_str2[i]);
 		}
 		i++;
 	}
