4ba451faf514e290687dc3522b10958c5a390c8a
ByteGab <gabrielfreiresimon@gmail.com>
2024-10-21 23:01:50 +0200

    change ft_strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -15,40 +15,28 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	new_len;
-	char	*result;
-	size_t	i;
 	size_t	start;
 	size_t	end;
+	size_t	new_len;
+	char	*result;
 
+	if (!s1 || !set)
+		return (NULL);
 	start = 0;
-	end = ft_strlen(s1) - 1;
+	end = ft_strlen(s1);
+	if (end == 0)
+		return (ft_strdup(""));
+	end--;
 	while (s1[start] != '\0' && ft_strchr(set, s1[start]))
-	{
 		start++;
-	}
 	while (end > start && ft_strchr(set, s1[end]))
-	{
 		end--;
-	}
 	if (start > end)
-	{
-		result = (char *)malloc(1);
-		result[0] = '\0';
-		return (result);
-	}
+		return (ft_strdup(""));
 	new_len = end - start + 1;
 	result = (char *)malloc(new_len + 1);
-	if (result == NULL)
-	{
+	if (!result)
 		return (NULL);
-	}
-	i = 0;
-	while (i < new_len)
-	{
-		result[i] = s1[start + i];
-		i++;
-	}
-	result[new_len] = '\0';
+	ft_strlcpy(result, &s1[start], new_len + 1);
 	return (result);
 }
