0f153a26a76f4e12e5edfd026340f5143a931d83
ByteGab <gabrielfreiresimon@gmail.com>
2024-10-22 23:17:24 +0200

    change ft_strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -17,8 +17,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	size_src;
 	size_t	i;
 
-	if (!src || !dst)
-		return (0);
 	i = 0;
 	size_src = ft_strlen(src);
 	if (size == 0)
