93a705188cc5caa2d0fa644aaf67ffaf36eb5075
ByteGab <gabrielfreiresimon@gmail.com>
2024-10-23 16:40:39 +0200

    change ft_memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -12,12 +12,12 @@
 
 #include "libft.h"
 
-void	*ft_memchr(const void *str, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	const unsigned char	*char_str;
 	size_t				i;
 
-	char_str = (unsigned char *)str;
+	char_str = (unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
